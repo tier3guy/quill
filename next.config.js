5a00ab10dd6b6ffaeba8b3a02ad4fc9e2bf298ce
Avinash Gupta <avinash.gupta@vils.ai>
2024-01-03 16:21:38 +0530

    config

--- next.config.js
+++ next.config.js
@@ -6,6 +6,15 @@ const nextConfig = {
 
     return config;
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "img.clerk.com",
+        port: "",
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
--- src/components/extended/Navbar.tsx
+++ src/components/extended/Navbar.tsx
@@ -1,57 +1,57 @@
-import React from "react";
 import Link from "next/link";
-import MaxWidthWrapper from "@/components/extended/MaxWidthWrapper";
-import { UserButton, auth } from "@clerk/nextjs";
-import { buttonVariants } from "../ui/button";
+import MaxWidthWrapper from "./MaxWidthWrapper";
+import { buttonVariants } from "@/components/ui/button";
+import {
+  currentUser,
+  SignInButton,
+  SignOutButton,
+  SignUpButton,
+} from "@clerk/nextjs";
 import { ArrowRight } from "lucide-react";
+import UserAccountNav from "@/components/extended/UserAccountNav";
+import MobileNav from "@/components/extended/MobileNav";
 
-const Navbar = () => {
-  const { userId } = auth();
+const Navbar = async () => {
+  const user = await currentUser();
 
   return (
     <nav className="sticky h-14 inset-x-0 top-0 z-30 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
       <MaxWidthWrapper>
         <div className="flex h-14 items-center justify-between border-b border-zinc-200">
-          <Link href={"/"} className="flex z-40 font-semibold text-3xl">
-            quill.
+          <Link href="/" className="flex z-40 font-semibold">
+            <span className="text-2xl">quill.</span>
           </Link>
 
-          {/* TODO : ADD MOBILE NAVBAR */}
+          <MobileNav isAuth={!!user} />
 
-          <div className="hidden sm:flex items-center space-x-4">
-            {!userId ? (
+          <div className="hidden items-center space-x-6 sm:flex">
+            {!user ? (
               <>
-                <Link
-                  href={"/pricing"}
-                  className="hover:bg-gray-100 px-3 py-1 rounded-sm"
-                >
-                  Pricing
-                </Link>
-                <Link
-                  href="/sign-in"
-                  className="hover:bg-gray-100 px-3 py-1 rounded-sm"
-                >
-                  Sign In
-                </Link>
-                <Link
-                  href={"/sign-up"}
-                  className={buttonVariants({
-                    size: "sm",
-                  })}
-                >
-                  Get Started <ArrowRight />
-                </Link>
+                <Link href="/pricing">Pricing</Link>
+                <SignInButton>Sign in</SignInButton>
+                <SignUpButton>
+                  <div className="flex items-center cursor-pointer">
+                    Get started <ArrowRight className="ml-1.5 h-5 w-5" />
+                  </div>
+                </SignUpButton>
               </>
             ) : (
               <>
-                <Link href={"/dashboard"}>Dashboard</Link>
-                <Link
-                  href={"/pricing"}
-                  className="hover:bg-gray-100 px-3 py-1 rounded-sm"
-                >
-                  Pricing
-                </Link>
-                <UserButton afterSignOutUrl="/" />
+                <Link href="/dashboard">Dashboard</Link>
+
+                <UserAccountNav
+                  name={
+                    !user.firstName || !user.lastName
+                      ? "Your Account"
+                      : `${user.firstName} ${user.lastName}`
+                  }
+                  email={
+                    user.emailAddresses.length > 0
+                      ? user.emailAddresses[0].emailAddress
+                      : ""
+                  }
+                  imageUrl={user.imageUrl ?? ""}
+                />
               </>
             )}
           </div>
